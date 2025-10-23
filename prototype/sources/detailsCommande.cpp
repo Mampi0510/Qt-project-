a0f018c496ece5ce05d07dbc0a8061a58f50469d
Mampi <tonimanantoky@gmail.com>
2025-10-23 10:09:14 +0300

    prototype deleted

--- prototype/sources/detailsCommande.cpp
+++
@@ -1,9 +0,0 @@
-#include "detailsCommande.h"
-#include <iostream>
-#include <string>
-
-void detailsCommande::afficherDetails(int idCommande, int idPlat, int quantite, double prixUnitaire) {
-    std::cout << "Affichage des détails de la commande." << std::endl;
-    std::cout << "ID Commande: " << idCommande << ", ID Plat: " << idPlat 
-              << ", Quantité: " << quantite << ", Prix Unitaire: " << prixUnitaire << std::endl;    
-}
